c36e9a647ccb82aa70d52abe2306d705057a5fdc
Amon Victor <amon.victor.develops@gmail.com>
2025-07-09 10:08:18 +0300

    Added the index route

--- server.ts
+++ server.ts
@@ -7,6 +7,13 @@ dotenv.config()
 
 const app: Express = express()
 
+app.get("/", (_req: Request, res: Response) => {
+    SendSuccessResponse(res, {
+        path: '/',
+        message: "welcome to blog-it API"
+    }, "index")
+})
+
 const PORT = process.env.PORT || 3500
 
 app.listen(PORT, () => {
