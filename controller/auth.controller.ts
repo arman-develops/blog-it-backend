dac2829773233ee8b3559b008d035899726be7eb
Amon Victor <amon.victor.develops@gmail.com>
2025-07-09 16:00:32 +0300

    Added the Auth Controller

---
+++ controller/auth.controller.ts
@@ -0,0 +1,42 @@
+import { log } from "console";
+import { Request, Response} from "express";
+import bcrypt from 'bcryptjs'
+import client from "../config/prisma.client";
+import { SendSuccessResponse } from "../utils/sucess.utils";
+import { SendErrorResponse } from "../utils/error.utils";
+
+const registerUser = async (req: Request, res: Response) => {
+    try {
+        const {firstName, lastName, email, password, username} = req.body
+        const saltRounds = 10
+        bcrypt.hash(password, saltRounds, async function(err, hash) {
+            if(err) {
+                SendErrorResponse(res, {
+                    data: {
+                        error: err
+                    }
+                }, "Something Went Wrong")
+                log("error hashing password")
+            }
+            const user = await client.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    password: hash as string,
+                    username
+                }
+            })
+            SendSuccessResponse(res, {user}, "Your profile was created success")
+            log("new user created")
+        })
+
+        
+    } catch (error) {
+        SendErrorResponse(res, {
+            error
+        }, "Error Creating your profile")
+    }
+}
+
+export {registerUser}
--- routes/users.route.ts
+++ routes/users.route.ts
@@ -1,45 +1,7 @@
-import { log } from "console";
-import { Request, Response, Router } from "express";
-import bcrypt from 'bcryptjs'
-import client from "../config/prisma.client";
-import { SendSuccessResponse } from "../utils/sucess.utils";
-import { SendErrorResponse } from "../utils/error.utils";
+import { Router } from "express"
+import { registerUser } from "../controller/auth.controller"
 
 const authRouter = Router()
-
-const registerUser = async (req: Request, res: Response) => {
-    try {
-        const {firstName, lastName, email, password, username} = req.body
-        const saltRounds = 10
-        bcrypt.hash(password, saltRounds, async function(err, hash) {
-            if(err) {
-                SendErrorResponse(res, {
-                    data: {
-                        error: err
-                    }
-                }, "Something Went Wrong")
-                log("error hashing password")
-            }
-            const user = await client.user.create({
-                data: {
-                    firstName,
-                    lastName,
-                    email,
-                    password: hash as string,
-                    username
-                }
-            })
-            SendSuccessResponse(res, {user}, "Your profile was created success")
-            log("new user created")
-        })
-
-        
-    } catch (error) {
-        SendErrorResponse(res, {
-            error
-        }, "Error Creating your profile")
-    }
-}
 
 authRouter.post("/register", registerUser)
 
